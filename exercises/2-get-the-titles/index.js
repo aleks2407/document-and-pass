8c2139e77e8f203723f0a2c7172c8ecb96218a26
Aleksandra <alex.shalimova@gmail.com>
2020-07-17 10:14:44 +0200

    Completed get-the-titles

--- exercises/2-get-the-titles/index.js
+++ exercises/2-get-the-titles/index.js
@@ -1,8 +1,43 @@
 /**
  *
+ * @param {Array} arr - an array to be checked
+ * @returns {Array} of strings - gets all the titles of the array and return them in a new array
+ *
+ * @example
+ * 
+ * const books = [{title: 'Book', author: 'Name'},{title: 'Book2', author: 'Name2'}]
+ * getTheTitles(books);
+ * console.log(books); // ['Book', 'Book2']
  */
-const getTheTitles = () => {
 
+
+
+const getTheTitles2 = (arr) => {
+    const bookTitles = [];
+    for (const item of arr) {
+    let title = item.title;
+    bookTitles.push(title);
+   } 
+   return bookTitles;
 }
+
+// Arrow function with MAP method
+const getTheTitles3 = (arr) => {
+    function getTitle(o) {
+        return o.title;
+    }
+    
+    return bookTitles = arr.map(getTitle);
+}
+
+// Arrow function with MAP method
+const getTheTitles4 = (arr) => {
+    return bookTitles = arr.map(o => o.title);
+}
+
+// Arrow function with MAP method
+const getTheTitles = arr => arr.map(o => o.title);
+
+
 
 module.exports = getTheTitles;
